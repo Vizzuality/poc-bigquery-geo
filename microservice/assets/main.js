f8cf6ab9b1b429c5e9c41ee9de242022dfe3598a
rrequero <rrequero@gmail.com>
2017-07-13 17:29:17 +0200

    update front

--- microservice/assets/main.js
+++ microservice/assets/main.js
@@ -7,8 +7,7 @@ var editor = null;
 require(['vs/editor/editor.main'], function () {
   editor = monaco.editor.create(document.getElementById('container'), {
     value: [
-      'SELECT year, SUM(number) as num FROM [bigquery-public-data:usa_names.usa_1910_2013]',
-      ' WHERE name = "William" GROUP BY year ORDER BY year '
+      'SELECT COUNT(*) FROM `alerts.fires`\n where viz_inside(longitude, latitude, \'[[[-55,49],[-39,49],[-39,53],[-55,53],[-55,49]]]\')'
     ].join('\n'),
     language: 'sql'
   });
@@ -19,9 +18,25 @@ var app = angular.module('poc-bigquery-geo', []);
 
 app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
   this.data = null;
-  this.exec = function (event) {
-    this.showLoading = true;
+  this.count = 0;
 
+  this.exampleIntersect = function($event) {
+    $event.preventDefault();
+    editor.setValue('SELECT COUNT(*) FROM `alerts.fires`\n where viz_intersect(the_geom, \'{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-51.50390625,63.78248603116502],[-39.0234375,63.78248603116502],[-39.0234375,67.941650035336],[-51.50390625,67.941650035336],[-51.50390625,63.78248603116502]]]}}\')');
+  }
+  this.exampleInside = function($event) {
+    $event.preventDefault();
+    editor.setValue('SELECT COUNT(*) FROM `alerts.fires`\n where viz_inside(longitude, latitude, \'[[[-55,49],[-39,49],[-39,53],[-55,53],[-55,49]]]\')');
+  }
+
+
+  this.exec = function (event) {
+    this.count = 0;
+    this.showLoading = true;
+    this.interval = setInterval(function() {
+      this.count++;
+      $scope.$apply();
+    }.bind(this), 1000);
 
     $http.get('/query?sql='+ editor.getValue()).then(function(body) {
       if (body && body.data && body.data.length > 0) {
@@ -31,22 +46,12 @@ app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         }
       }
       this.showLoading = false;
+      clearInterval(this.interval);
     }.bind(this), function(err){
         alert('Query not valid \n', err.error);
       this.showLoading = false;
-    });
-
-    // setTimeout(function () {
-    //   this.data = {
-    //     rows: [{
-    //       name: 'ra',
-    //       age: 29
-    //     }],
-    //     headers: ['name', 'age']
-    //   }
-
-    //   $scope.$apply();
-    // }.bind(this), 2000);
+      clearInterval(this.interval);
+    }.bind(this));
     event.preventDefault();
 
   }
